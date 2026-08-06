2ed520e7ac4b5ba932cd954aec029fd39156e926
Eli Holmes <eeholmes@uw.edu>
2026-08-06 03:01:13 +0000

    Remove create_zarr.py (old eager preprocessing, replaced by build_standardized_lazy_new)

--- mindthegap/__init__.py
+++ mindthegap/__init__.py
@@ -1,5 +1,4 @@
 # Re-export the primary functions (explicit, stable API)
-from .create_zarr import create_zarr, data_preprocessing
 from .utils import (
     crop_to_multiple,
     unstdize,
@@ -39,8 +38,6 @@ from .selfsup import (
 from . import viz  # users can do: from mindthegap import viz; viz.plot_prediction_observed(...)
 
 __all__ = [
-    "create_zarr",
-    "data_preprocessing",
     "crop_to_multiple",
     "unstdize",
     "compute_mae",
