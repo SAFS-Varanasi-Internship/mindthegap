e88eff85d4c2bba4e1d52f643abbf5e12c4820eb
Eli Holmes <eeholmes@uw.edu>
2026-08-06 21:25:13 +0000

    cleanup mindthegap functions

--- mindthegap/__init__.py
+++ mindthegap/__init__.py
@@ -6,33 +6,8 @@ from .utils import (
     compute_mse,
     make_tf_gen,
     build_standardized_lazy,
-    build_standardized_lazy_new,
     make_xbatcher,
     UNet,
-)
-# Spatial patching + tiled inference
-from .patching import (
-    grid_positions,
-    random_positions,
-    coast_mask,
-    coast_positions,
-    coast_weight,
-    coast_weighted_positions,
-    make_crops,
-    tiled_predict,
-)
-# Self-supervised gap-fill training pieces (masked loss, fake clouds, splits, metric)
-from .selfsup import (
-    masked_mse,
-    masked_mae,
-    target_with_mask,
-    keep_cloud_blobs,
-    synthetic_cloud_cube,
-    build_pace_channels,
-    season_of,
-    season_blocked_split,
-    contiguous_split,
-    fake_cloud_mae,
 )
 # Expose the viz module as a submodule (lazy import by users)
 from . import viz  # users can do: from mindthegap import viz; viz.plot_prediction_observed(...)
@@ -44,28 +19,7 @@ __all__ = [
     "compute_mse",
     "make_tf_gen",
     "build_standardized_lazy",
-    "build_standardized_lazy_new",
     "make_xbatcher",
     "UNet",
-    # patching
-    "grid_positions",
-    "random_positions",
-    "coast_mask",
-    "coast_positions",
-    "coast_weight",
-    "coast_weighted_positions",
-    "make_crops",
-    "tiled_predict",
-    # selfsup
-    "masked_mse",
-    "masked_mae",
-    "target_with_mask",
-    "keep_cloud_blobs",
-    "synthetic_cloud_cube",
-    "build_pace_channels",
-    "season_of",
-    "season_blocked_split",
-    "contiguous_split",
-    "fake_cloud_mae",
     "viz",
 ]
